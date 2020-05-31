ba2dd2f6f9c2a4caf789a09a61cde1160ec25bc3
vlad07072002li@gmail.com <vlad07072002li@gmail.com>
2020-05-31 18:26:25 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication12/ConsoleApplication12.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+
+std::string foo(const std::string& str)
+{
+	std::string ret;
+	for (auto ch : str)
+	{
+		ret.push_back(ch);
+		if (ch == ' ') ret.push_back(ch);
+	}
+	return ret;
+}
+
+int main()
+{
+	std::string str{ "o n e t o o sana string..." };
+	foo(str).swap(str);
+	std::cout << str;
+}
